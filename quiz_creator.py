9308075b7134b5953b9e491a81389487003a15d8
justinroyse <justinroyselsolomon@gmail.com>
2025-04-11 10:11:10 +0800

    Prototyped main program logic in while loop

--- quiz_creator.py
+++ quiz_creator.py
@@ -3,17 +3,18 @@ def main_menu():
     print("\n********** Welcome to Quiz Creator! **********")
     print("1. Create Quiz")
     print("2. Exit and save to file")
-    print("************************************************")
+    print("**********************************************")
 
 
 #Initialize list to store questions and answers to append in
 question_data = []
 
 #Initialize a while loop to ask for user input until the user choose to exit
+while True:
+    main_menu()
+    choice = int(input("Choice (1 and 2): "))
 
 #Initialize user interface main menu 
 
 #Store and write user input into a text file format
 
-#test
-main_menu()
