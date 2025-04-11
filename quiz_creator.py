74b7c80ee5db1fafaee7003322edd8aa0d56e375
justinroyse <justinroyselsolomon@gmail.com>
2025-04-11 10:07:03 +0800

    Prototyped displaying main menu

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,4 +1,10 @@
 #initialize main menu function() to display main menu
+def main_menu():
+    print("\n********** Welcome to Quiz Creator! **********")
+    print("1. Create Quiz")
+    print("2. Exit and save to file")
+    print("************************************************")
+
 
 #Initialize list to store questions and answers to append in
 
@@ -7,3 +13,6 @@
 #Initialize user interface main menu 
 
 #Store and write user input into a text file format
+
+#test
+main_menu()
