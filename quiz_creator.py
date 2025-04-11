51b4b82a3268258685c27c39c7cc0b3b1f9807ef
justinroyse <justinroyselsolomon@gmail.com>
2025-04-11 10:00:59 +0800

    Updated Pseudocodes to display main menu and be incorporated in the while loop, Added list Pseudocode

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,3 +1,9 @@
-#initialize a while loop to ask for user input until the user choose to exit
-#initialize user interface main menu 
+#initialize main menu function() to display main menu
+
+#Initialize list to store questions and answers to append in
+
+#Initialize a while loop to ask for user input until the user choose to exit
+
+#Initialize user interface main menu 
+
 #Store and write user input into a text file format
