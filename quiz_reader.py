9f892e5f58921c41f391ad25ab2736f50899040f
justinroyse <justinroyselsolomon@gmail.com>
2025-05-09 17:17:22 +0800

    Load questions from the quiz file and return a list of question dictionaries

--- quiz_reader.py
+++ quiz_reader.py
@@ -1,6 +1,11 @@
 # Import module (random) to load the questions randomly for the user to answer
+import random
 
 # Create user defined function for loading the quiz file
+def reader(filename):
+    with open(filename, "r") as file:
+        content = file.read()
+
 
 # Create user defined function for quiz game
 
